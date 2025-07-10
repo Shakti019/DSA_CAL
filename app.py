f1db584699578e4d8a80dd28e1ef01274c81ecdc
Shakti Singh <149115655+Shakti019@users.noreply.github.com>
2025-07-10 17:36:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -212,5 +212,7 @@ def equation_solver():
     except Exception as e:
         return jsonify({'error': str(e)})
 
-if __name__ == '__main__':
-    app.run(debug=True) 
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port) 
